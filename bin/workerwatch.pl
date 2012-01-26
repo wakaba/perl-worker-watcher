cdb1dc0d564fe2060bd9034dcd1e19bc1ed9f31f
wakabatan <wakabatan@localhost.localdomain>
2012-01-26 18:39:29 +0900

    watcher

---
+++ bin/workerwatch.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Path::Class;
+use lib glob file(__FILE__)->dir->parent->subdir ('modules', '*', 'lib')->stringify;
+use Dongry::Database;
+use Data::Dumper;
+use Getopt::Long;
+
+my $workerdb_dsn;
+my $message;
+my $CountThreshold = 100;
+GetOptions(
+    'count-threshold=s' => \$CountThreshold,
+    'dsn=s' => \$workerdb_dsn,
+    'error-message=s' => \$message,
+) or die "Usage: $0 --dsn=dsn --error-message=message\n";
+
+my $db = Dongry::Database->new(
+    sources => {
+        default => {
+            dsn => $workerdb_dsn,
+        },
+    },
+);
+
+my $reported = {};
+$db->execute ('SELECT funcid, COUNT(*) AS count FROM job GROUP BY funcid')->each(sub {
+    if ($_->{count} >= $CountThreshold) {
+        $reported->{$_->{funcid}} = $_;
+    }
+});
+
+if (keys %$reported) {
+    $db->execute (
+        'SELECT funcid, funcname FROM funcmap WHERE funcid IN (:funcid)',
+        {funcid => [map { $_->{funcid} } values %$reported]},
+    )->each(sub {
+        $reported->{$_->{funcid}}->{funcname} = $_->{funcname};
+    });
+}
+
+$workerdb_dsn =~ s/\b(?:user|password)=[^;]*//g;
+
+warn Dumper {
+    time => time,
+    localtime => scalar localtime,
+    workerdb => $workerdb_dsn,
+    jobs => [sort { $b->{count} <=> $a->{count} } values %$reported],
+    message => $message,
+};
+
+=head1 USAGE
+
+  $ perl path/to/workerwatch.pl \
+        --dsn "dsn:..." \
+        --error-message "Too many worker jobs > id:wakabatan" \
+        --count-threshold 200
+
+=head1 AUTHOR
+
+Wakaba (id:wakabatan) <wakabatan@hatena.ne.jp>.
+
+=head1 LICENSE
+
+Copyright 2012 Hatena <http://www.hatena.com/>.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
